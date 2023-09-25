4467092fc78a6c8308e8d62aec01799bcd578f6d
Marllon Costa <marllonsc@gmail.com>
2023-09-25 09:04:08 -0300

    add ReturnBooks componente

--- libray-app/03-frontend/react-library/src/App.tsx
+++ libray-app/03-frontend/react-library/src/App.tsx
@@ -2,12 +2,14 @@ import React from 'react';
 import './App.css';
 import { Navbar } from './layouts/NavbarAndfooter/Navbar';
 import { ExplorerTopBooks } from './layouts/HomePage/ExplorerTopBooks';
+import { Carousel } from './layouts/HomePage/Carousel';
 
 function App() {
   return (
     <div>
       <Navbar></Navbar>
       <ExplorerTopBooks></ExplorerTopBooks>
+      <Carousel></Carousel>
     </div>
   );
 }
