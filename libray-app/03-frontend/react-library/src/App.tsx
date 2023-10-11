748772481f55d87afe545e2a3f2c0d4db118787a
Marllon Costa <marllonsc@gmail.com>
2023-10-11 09:46:38 -0300

    create and use Book Search

--- libray-app/03-frontend/react-library/src/App.tsx
+++ libray-app/03-frontend/react-library/src/App.tsx
@@ -3,12 +3,15 @@ import './App.css';
 import { Navbar } from './layouts/NavbarAndfooter/Navbar';
 import { Footer } from './layouts/NavbarAndfooter/Footer';
 import { HomePage } from './layouts/HomePage/HomePage';
+import { SearchBook } from './layouts/SearchBooksPage/components/SeachBook';
+import { SearchBooksPages } from './layouts/SearchBooksPage/SearchBooksPages';
 
 export const  App = () => {
   return (
     <div>
       <Navbar></Navbar>
-      <HomePage></HomePage>
+      {/* <HomePage></HomePage> */}
+      <SearchBooksPages></SearchBooksPages>
       <Footer></Footer>
     </div>
   );
--- libray-app/03-frontend/react-library/src/layouts/SearchBooksPage/SearchBooksPages.tsx
+++ libray-app/03-frontend/react-library/src/layouts/SearchBooksPage/SearchBooksPages.tsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from "react";
 import BookModel from "../../models/BookModels";
 import { forEachChild } from "typescript";
 import { SpinnerLoading } from "../Utils/SpinnerLoading";
+import { SearchBook } from "./components/SeachBook";
 
 export const SearchBooksPages = () => {
 
@@ -61,6 +62,65 @@ export const SearchBooksPages = () => {
       }
 
       return (
-        <div></div>
+        <div>
+          <div className="container">
+            <div>
+            <div className="row mt-5">
+              <div className="col-6">
+                <div className="d-flex">
+                  <input className="form-control me-2" type="search" />
+                  <button className="btn btn-outline-success">
+                    Search
+                  </button>
+                </div>
+              </div>
+              <div className="col-4">
+                <div className="dropdown">
+                  <button className="btn btn-secondary dropdown-toggle" type="button"
+                  id="dropdownButton1" data-bs-toggle="dropdown" aria-expanded="false">
+                      Category
+                  </button>
+                  <ul className="dropdown-menu" aria-labelledby="dropdownButton1">
+                    <li>
+                      <a className="dropdown-item" href="#">
+                        All
+                      </a>
+                    </li>
+                    <li>
+                      <a className="dropdown-item" href="#">
+                        Front End
+                      </a>
+                    </li>
+                    <li>
+                      <a className="dropdown-item" href="#">
+                        Back End
+                      </a>
+                    </li>
+                    <li>
+                      <a className="dropdown-item" href="#">
+                        Data
+                      </a>
+                    </li>
+                    <li>
+                      <a className="dropdown-item" href="#">
+                        DevOps
+                      </a>
+                    </li>
+                  </ul>
+                </div>
+              </div>
+              <div className="mt-3">
+                <h5>Number of Results: (22)</h5>
+              </div>
+              <p>
+                1 to 5 of 22 items:
+              </p>
+              {books.map(book => (
+                <SearchBook book={book} key={book.id} />
+              ))}
+            </div>
+            </div>
+          </div>
+        </div>
       )
 }
