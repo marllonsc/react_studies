3763f23c8a151ca04a0edb7a55c2cd43fabb1e95
Marllon Costa <marllonsc@gmail.com>
2023-10-31 11:23:05 -0300

    checkout page add

--- libray-app/03-frontend/react-library/src/layouts/BookkCheckoutPage/BookCheckoutPage.tsx
+++ libray-app/03-frontend/react-library/src/layouts/BookkCheckoutPage/BookCheckoutPage.tsx
@@ -1,15 +1,106 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import BookModel from "../../models/BookModels";
+import { SpinnerLoading } from "../Utils/SpinnerLoading";
 
 export const BookCheckoutPage = () => {
 
     const [book, setBook] = useState<BookModel>();
-    const [isLoadingBook, setIsLoadingBook ] = useState(true);
+    const [isLoading, setIsLoading] = useState(true);
+    const [httpError, setHttpError] = useState(null);
 
-    return(
+    const bookId = (window.location.pathname).split('/')[2];
+
+    const ip_linux_virtual = "http://192.168.49.2:30979/";
+    const ip_linux = "http://192.168.58.2:31529/";
+
+    useEffect(() => {
+        const fetchBooks = async () => {
+            const baseUrl: string = `${ip_linux_virtual}api/books/${bookId}`;
+
+            const url: string = `${baseUrl}?page=0&size=9`;
+
+            const response = await fetch(url, { mode: 'cors' });
+
+            if (!response.ok) {
+                throw new Error('Something went wrong!');
+            }
+
+            const responseJson = await response.json();
+
+            //const reponseData = responseJson._embedded.books;
+
+            const loadedBook: BookModel = {
+                id: responseJson.id,
+                title: responseJson.title,
+                author: responseJson.author,
+                description: responseJson.description,
+                copies: responseJson.copies,
+                copiesAvailable: responseJson.copiesAvailable,
+                category: responseJson.category,
+                img: responseJson.img,
+            }
+
+            setBook(loadedBook);
+            setIsLoading(false);
+        };
+        fetchBooks().catch((error: any) => {
+            setIsLoading(false);
+            setHttpError(error.message);
+        })
+    }, []);
+
+    if (isLoading) {
+        return (
+            <SpinnerLoading></SpinnerLoading>
+        )
+    }
+
+    if (httpError) {
+        return (
+            <div className="container m-5">
+                <p>{httpError}</p>
+            </div>
+        )
+    }
+
+    return (
         <div>
-            <h3>Hi World!</h3>
-
-        </div>
+            <div className="container d-none d-lg-block">
+                <div className="row mt-5">
+                    <div className="col-sm-2 col-md-2">
+                        {book?.img ?
+                            <img src={book?.img} width='226' height='349' alt='Book' />
+                            :
+                            <img src={require('./../../Images/BooksImages/book-luv2code-1000.png')} width='226' height='349' alt='Book' />
+                        }
+                    </div>
+                    <div className="col-4 col-md-4 container">
+                        <div className="ml-2">
+                            <h2>{book?.title}</h2>
+                            <h5 className="text-primary">{book?.author}</h5>
+                            <p className="lead">{book?.description}</p>
+                        </div>
+                    </div>
+                </div>
+                <hr></hr>
+            </div>
+            <div className="container d-lg-none mt-5">
+                <div className="d-flex justify-content-center align-items-center">
+                    {book?.img ?
+                        <img src={book?.img} width='226' height='349' alt='Book' />
+                        :
+                        <img src={require('./../../Images/BooksImages/book-luv2code-1000.png')} width='226' height='349' alt='Book' />
+                    }
+                </div>
+                <div className="mt-4">
+                    <div className="ml-2">
+                        <h2>{book?.title}</h2>
+                        <h5 className="text-primary">{book?.author}</h5>
+                        <p className="lead">{book?.description}</p>
+                    </div>
+                </div>
+                <hr></hr>
+            </div>
+        </div >
     );
 }
