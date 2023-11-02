c56b965fb59feec432e072dc33695c9a3d24eff1
Marllon Costa <marllonsc@gmail.com>
2023-11-02 09:47:25 -0300

    add review search by book

--- libray-app/02-backend/spring-boot-library/src/main/java/com/luv2code/springbootlibrary/dao/ReviewRepository.java
+++ libray-app/02-backend/spring-boot-library/src/main/java/com/luv2code/springbootlibrary/dao/ReviewRepository.java
@@ -1,9 +1,15 @@
 package com.luv2code.springbootlibrary.dao;
 
+import java.awt.print.Pageable;
+
+import org.springframework.data.domain.Page;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.luv2code.springbootlibrary.entity.Review;
 
 public interface ReviewRepository extends JpaRepository<Review, Long>{
+	
+	Page<Review> findByBookId(@RequestParam("book_id") long bookId, Pageable pageable);
 
 }
