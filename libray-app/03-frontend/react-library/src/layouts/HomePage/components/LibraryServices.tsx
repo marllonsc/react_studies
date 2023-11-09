6ded18fc9652e8a1f5bb8acf8532bab75d83c7d4
Marllon Costa <marllonsc@gmail.com>
2023-11-09 09:34:25 -0300

    put the links on home page to work

--- libray-app/03-frontend/react-library/src/layouts/HomePage/components/LibraryServices.tsx
+++ libray-app/03-frontend/react-library/src/layouts/HomePage/components/LibraryServices.tsx
@@ -1,5 +1,11 @@
+import { useOktaAuth } from "@okta/okta-react";
+import { Link } from "react-router-dom";
+
 export const LibraryService = () => {
-    return(
+
+    const { authState } = useOktaAuth();
+
+    return (
         <div className="container my-5">
             <div className="row p-4 align-items-center border shadow-lf">
                 <div className="col-lg-7 p-3">
@@ -11,9 +17,13 @@ export const LibraryService = () => {
                         send our library admin's a personal message!
                     </p>
                     <div className="d-grid gap-2 justify-content-md-start mb-4 mb-lg-3">
-                        <a className="btn main-color ntn-lg text-white" href="#">
-                            Sing up
-                        </a>
+                        {authState?.isAuthenticated ?
+                            <Link type="button" className="btn main-color btn-lg px-4 me-md-2 fw-bold text-white" to='#'>
+                                Library Services
+                                 </Link>
+                            :
+                            <Link className="btn main-color btn-lg text-white" to="/login">Sign up</Link>
+                        }
                     </div>
                 </div>
                 <div className="col-lg-4 offset-lg-1 shadow-lg lost-image"></div>
