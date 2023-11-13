9801dbc7515e57ccb1d0a43e28db4f3581fcab1a
Marllon Costa <marllonsc@gmail.com>
2023-11-13 10:18:46 -0300

    commit security back-end

---
+++ libray-app/02-backend/spring-boot-library/src/main/java/com/luv2code/springbootlibrary/config/SecurityConfiguration.java
@@ -0,0 +1,37 @@
+package com.luv2code.springbootlibrary.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.accept.ContentNegotiationStrategy;
+import org.springframework.web.accept.HeaderContentNegotiationStrategy;
+
+import com.okta.spring.boot.oauth.Okta;
+
+@Configuration
+public class SecurityConfiguration {
+
+	@Bean
+	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+		
+		//Disable Cross site Request forgery
+		http.csrf().disable();
+		
+		// Protect endpoints at /api/<type>/secure
+		http.authorizeRequests(configurer -> configurer.requestMatchers("/api/books/secure/**").authenticated())
+		.oauth2ResourceServer().jwt();
+		
+		//add CORS filters
+		http.cors();
+		
+		//add content negotiation strategy
+		http.setSharedObject(ContentNegotiationStrategy.class, new HeaderContentNegotiationStrategy());
+		
+		
+		//	Force a now-empty response body for 401's to meake the response friendly
+		Okta.configureResourceServer401ResponseBody(http);
+		
+		return http.build();
+	}
+}
