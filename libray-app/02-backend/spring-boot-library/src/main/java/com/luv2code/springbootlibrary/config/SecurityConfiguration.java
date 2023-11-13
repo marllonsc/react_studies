946fee5f86e37ef252359a1effd35aede9da7268
Marllon Costa <marllonsc@gmail.com>
2023-11-13 10:43:18 -0300

    commit fix bug

--- libray-app/02-backend/spring-boot-library/src/main/java/com/luv2code/springbootlibrary/config/SecurityConfiguration.java
+++ libray-app/02-backend/spring-boot-library/src/main/java/com/luv2code/springbootlibrary/config/SecurityConfiguration.java
@@ -1,8 +1,13 @@
 package com.luv2code.springbootlibrary.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.authority.mapping.SimpleAuthorityMapper;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.web.accept.ContentNegotiationStrategy;
 import org.springframework.web.accept.HeaderContentNegotiationStrategy;
@@ -10,7 +15,9 @@ import org.springframework.web.accept.HeaderContentNegotiationStrategy;
 import com.okta.spring.boot.oauth.Okta;
 
 @Configuration
+@EnableWebSecurity
 public class SecurityConfiguration {
+	
 
 	@Bean
 	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
@@ -34,4 +41,7 @@ public class SecurityConfiguration {
 		
 		return http.build();
 	}
+
+
+
 }
