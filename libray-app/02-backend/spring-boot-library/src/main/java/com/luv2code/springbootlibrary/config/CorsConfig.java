75b88a1102cbc3916a29a9daf47aee8c4085a132
Marllon Costa <marllonsc@gmail.com>
2024-01-17 11:36:10 -0300

    add cors to securtiy to all oringis

--- libray-app/02-backend/spring-boot-library/src/main/java/com/luv2code/springbootlibrary/config/CorsConfig.java
+++ libray-app/02-backend/spring-boot-library/src/main/java/com/luv2code/springbootlibrary/config/CorsConfig.java
@@ -3,15 +3,17 @@ package com.luv2code.springbootlibrary.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
+@EnableWebMvc
 public class CorsConfig implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/api/**")
-        		.allowedOriginPatterns("*")
+                .allowedOrigins("*")
                 .allowedMethods("GET", "POST", "PUT", "DELETE")
                 .allowedHeaders("Authorization", "Content-Type")
                 .exposedHeaders("Authorization")
