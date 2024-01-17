b92f151022e9046f6467ef97080456d18d7493d7
Marllon Costa <marllonsc@gmail.com>
2024-01-17 12:15:22 -0300

    tray to fix cors problems

--- libray-app/02-backend/spring-boot-library/src/main/java/com/luv2code/springbootlibrary/config/CorsConfig.java
+++ libray-app/02-backend/spring-boot-library/src/main/java/com/luv2code/springbootlibrary/config/CorsConfig.java
@@ -11,7 +11,8 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/api/**")
-                .allowedOrigins("http://localhost:3000")
+                //.allowedOrigins("http://localhost:3000")
+        		.allowedOriginPatterns("*")
                 .allowedMethods("GET", "POST", "PUT", "DELETE")
                 .allowedHeaders("Authorization", "Content-Type")
                 .exposedHeaders("Authorization")
