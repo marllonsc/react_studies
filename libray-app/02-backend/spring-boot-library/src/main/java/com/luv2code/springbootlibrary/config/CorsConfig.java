bd5a9f909319367fbce3729dd73b41bda4ecab1e
Marllon Costa <marllonsc@gmail.com>
2024-01-17 11:21:01 -0300

    add cors to securtiy

---
+++ libray-app/02-backend/spring-boot-library/src/main/java/com/luv2code/springbootlibrary/config/CorsConfig.java
@@ -0,0 +1,20 @@
+package com.luv2code.springbootlibrary.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/api/**")
+                .allowedOrigins("*")
+                .allowedMethods("GET", "POST", "PUT", "DELETE")
+                .allowedHeaders("Authorization", "Content-Type")
+                .exposedHeaders("Authorization")
+                .allowCredentials(true);
+    }
+}
