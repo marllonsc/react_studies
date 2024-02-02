ea88cbf6b61376fda0ece2b3d848aab54bed3d38
Marllon Costa <marllonsc@gmail.com>
2024-02-02 10:42:49 -0300

    add loans page

---
+++ libray-app/03-frontend/react-library/src/layouts/ShelfPage/components/Loans.tsx
@@ -0,0 +1,144 @@
+import { useOktaAuth } from "@okta/okta-react"
+import { useEffect, useState } from "react";
+import ShelfCurrentLoans from "../../../models/ShelfCurrentLoans";
+import { SpinnerLoading } from "../../Utils/SpinnerLoading";
+import { Link } from "react-router-dom";
+
+export const Loans = () => {
+
+    const ip_linux_virtual = "http://192.168.49.2:30979/";
+    // const ip_linux = "http://localhost:8080/";
+    const ip_linux = "http://192.168.0.102:8083/";
+
+    const { authState } = useOktaAuth();
+    const [httpError, setHttpError] = useState(null);
+
+
+    const [shelfCurrentLoans, setShelfCurrentLoans] = useState<ShelfCurrentLoans[]>([]);
+    const [isLoadingUserLoans, setIsLoadingUserLoans] = useState(true);
+
+    useEffect(() => {
+        const fetchUserCurrentLoans = async () => {
+            if (authState && authState.isAuthenticated) {
+                const url = `${ip_linux}api/books/secure/currentloans`;
+                const requestOptions = {
+                    method: 'GET',
+                    headers: {
+                        Authorization: `Bearer ${authState.accessToken?.accessToken}`,
+                        'Content-Type': 'application/json'
+                    }
+                };
+                const shelfCurrentLoansResponse = await fetch(url, requestOptions);
+                if (!shelfCurrentLoansResponse.ok) {
+                    throw new Error("Something went wrong");
+                }
+                const shelfCurrentLoansResponseJson = await shelfCurrentLoansResponse.json();
+                setShelfCurrentLoans(shelfCurrentLoansResponseJson);
+            }
+            setIsLoadingUserLoans(false);
+
+        }
+        fetchUserCurrentLoans().catch((error: any) => {
+            setIsLoadingUserLoans(false);
+            setHttpError(error.message);
+        })
+        window.scrollTo(0, 0);
+    }, [authState]);
+
+    if (isLoadingUserLoans) {
+        return (<SpinnerLoading></SpinnerLoading>)
+    }
+
+    if (httpError) {
+        return (
+            <div className="container m-5">
+                <p>{httpError}</p>
+            </div>
+        )
+    }
+
+    return (
+        <div>
+            <div>
+                {/* Desktop */}
+                <div className="d-none d-lg-block mt-2">
+                    {shelfCurrentLoans.length > 0 ?
+
+                        <>
+                            <h5>Current Loans:</h5>
+
+                            {shelfCurrentLoans.map(shelfCurrentLoan => (
+                                <div key={shelfCurrentLoan.book.id}>
+                                    <div className="row mt-3 mb-3">
+                                        <div className="col-4 col-md-4 container">
+                                            {shelfCurrentLoan.book?.img ?
+                                                <img src={shelfCurrentLoan.book?.img} width="226" height="349" alt="Book" />
+                                                :
+                                                <img src={require("./../../../Images/BooksImages/book-luv2code-1000.png")} width="226" height="349" alt="Book" />
+                                            }
+                                        </div>
+                                        <div className="card col-3 col-md-3 container d-flex">
+                                            <div className="card-body">
+                                                <div className="mt-3">
+                                                    <h4>Loan Options</h4>
+                                                    {shelfCurrentLoan.daysLeft > 0 &&
+                                                        <p className="text-secondary">
+                                                            Due in {shelfCurrentLoan.daysLeft} days.
+                                                        </p>
+                                                    }
+                                                    {shelfCurrentLoan.daysLeft === 0 &&
+                                                        <p className="text-success">
+                                                            Due Today
+                                                        </p>}
+                                                    {shelfCurrentLoan.daysLeft < 0 &&
+                                                        <p className="text-danger">
+                                                            Past due by {shelfCurrentLoan.daysLeft} days.
+                                                        </p>}
+
+                                                    <div className="list-group mt-3">
+                                                        <button className="list-group-item list-group-item-action"
+                                                            aria-current='true' data-bs-toggle='modal' data-bs-target={`#modal${shelfCurrentLoan.book.id}`}>
+                                                            Manage Loan
+                                                        </button>
+                                                        <Link to={'search'} className="list-group-item list-group-item-action">
+                                                            Search more books?
+                                                        </Link>
+                                                    </div>
+                                                </div>
+                                                <hr></hr>
+                                                <p className="mt-3">
+                                                    Help other find their adventure by reeviewing your loan.
+                                                </p>
+                                                <Link className="btn btn-primary" to={`/checkout/${shelfCurrentLoan.book.id}`}>
+                                                    Leave a reeview
+                                                </Link>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <hr></hr>
+                                </div>
+                            ))}
+
+                        </> :
+                        <>
+                            <h3 className="mt-3">
+                                Currently no Loans
+                            </h3>
+                            <Link className="btn btn-primary" to={`search`}>
+                                Search for a new book
+                            </Link>
+                        </>}
+                </div>
+
+
+            </div>
+
+            <div>
+                {/* Mobile */}
+
+
+
+            </div>
+        </div>
+    );
+}
---
+++ libray-app/03-frontend/react-library/src/models/ShelfCurrentLoans.ts
@@ -0,0 +1,15 @@
+import BookModel from "./BookModels";
+
+class ShelfCurrentLoans{
+
+    book: BookModel;
+    daysLeft: number;
+
+    constructor( book: BookModel, daysLeft: number ){
+        this.book = book;
+        this.daysLeft = daysLeft;
+    }
+
+}
+
+export default ShelfCurrentLoans;
