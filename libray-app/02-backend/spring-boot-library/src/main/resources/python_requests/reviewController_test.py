b121a96a580ef7c7735cbb67cd7db2f8cfb04403
Marllon Costa <marllonsc@gmail.com>
2024-02-15 08:43:26 -0300

    change server ip

--- libray-app/02-backend/spring-boot-library/src/main/resources/python_requests/reviewController_test.py
+++ libray-app/02-backend/spring-boot-library/src/main/resources/python_requests/reviewController_test.py
@@ -1,7 +1,7 @@
 import requests
 
 # Replace the URL with your actual endpoint const ip_linux = "http://192.168.0.104:8083/";
-#url = "http://localhost:8080/api/reviews/secure"
+#url = "http://192.168.0.103:8083/api/reviews/secure"
 #url = "http://192.168.0.104:8083/api/reviews/secure"
 url = "http://192.168.0.104:8083/api/reviews/secure/user/book"
 
