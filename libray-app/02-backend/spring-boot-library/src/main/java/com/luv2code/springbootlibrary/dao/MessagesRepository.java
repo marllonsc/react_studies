d9e88e7dc71f72ca9e4810d2b584859a53f8f508
marllon <marllon@ibm.com>
2024-02-27 10:17:02 -0300

    change importe on messages repository

--- libray-app/02-backend/spring-boot-library/src/main/java/com/luv2code/springbootlibrary/dao/MessagesRepository.java
+++ libray-app/02-backend/spring-boot-library/src/main/java/com/luv2code/springbootlibrary/dao/MessagesRepository.java
@@ -1,7 +1,7 @@
 package com.luv2code.springbootlibrary.dao;
 
-import org.springframework.boot.autoconfigure.data.web.SpringDataWebProperties.Pageable;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.web.bind.annotation.RequestParam;
 
